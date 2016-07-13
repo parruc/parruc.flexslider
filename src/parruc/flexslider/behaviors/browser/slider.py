fe4a53bec8d1340ca4036d0c2003a0ece5a5b170
root <root@aria>
2016-07-13 14:45:34 +0200

    fixed link bug

--- src/parruc/flexslider/behaviors/browser/slider.py
+++ src/parruc/flexslider/behaviors/browser/slider.py
@@ -7,4 +7,5 @@ from plone.app.layout.viewlets import common as base
 class SliderViewlet(base.ViewletBase):
 
     def slides(self):
+        import pdb; pdb.set_trace()
         return self.context.slides
